263d20051e809749dd88b216d2857025110b70bf
Вера Бондарь <v.bondar@cft.ru>
2019-07-31 10:50:19 +0700

    (add) - добавлена реализация методов в классе хранилища ArrayStorage (в виде очереди) и  реализация класса Resume (элементы, из которых состоит хранилище ArrayStorage)

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,26 +5,55 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        for (int i = 0; i <= size(); i++)
+            storage[i] = null;
     }
 
     void save(Resume r) {
+        if (get(r.getUuid()) != null) return;
+        storage[size()] = r;
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < size(); i++) {
+            if (storage[i].getUuid() == uuid)
+                return storage[i];
+        }
         return null;
     }
 
     void delete(String uuid) {
+        if (get(uuid) == null) return;
+
+        int number = 0;
+        for (int i = 0; i < size(); i++)
+            if (storage[i].getUuid() == uuid) {
+                number = i;
+                break;
+            }
+        for (int i = number; i < size(); i++)
+            storage[i] = storage[i + 1];
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        if (size() == 0)
+            return new Resume[0];
+
+        Resume[] shortStroage = new Resume[size()];
+        for (int i = 0; i < size(); i++)
+            shortStroage[i] = storage[i];
+        return shortStroage;
     }
 
     int size() {
-        return 0;
+        int count = 0;
+        for (Resume resume : storage) {
+            if (resume != null)
+                count++;
+        }
+        return count;
     }
 }
--- src/Resume.java
+++ src/Resume.java
@@ -4,10 +4,25 @@
 public class Resume {
 
     // Unique identifier
-    String uuid;
+    private String uuid;
 
     @Override
     public String toString() {
+        return getUuid();
+    }
+
+    public Resume(String uuid) {
+        this.setUuid(uuid);
+    }
+
+    public Resume() {
+    }
+
+    public String getUuid() {
         return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
     }
 }
