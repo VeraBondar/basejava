563593aa7cfe6b9ce92e0e89bc701004f1c81816
VeraBondar <53425256+VeraBondar@users.noreply.github.com>
2019-08-16 09:20:50 +0700

    (fix) - ещё раз исправила замечания

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -6,20 +6,21 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
-        for (int i = 0; i < size(); i++)
+        for (int i = 0; i < size; i++) {
             storage[i] = null;
+        }
         size = 0;
     }
 
     void save(Resume r) {
         if (get(r.getUuid()) != null) return;
-        storage[size()] = r;
+        storage[size] = r;
         size++;
     }
 
     Resume get(String uuid) {
-        for (int i = 0; i < size(); i++) {
-            if (storage[i].getUuid() == uuid){
+        for (int i = 0; i < size; i++) {
+            if (storage[i].getUuid() == uuid) {
                 return storage[i];
             }
         }
@@ -30,13 +31,13 @@ public class ArrayStorage {
         if (get(uuid) == null) return;
 
         int number = 0;
-        for (int i = 0; i < size(); i++){
+        for (int i = 0; i < size; i++) {
             if (storage[i].getUuid() == uuid) {
                 number = i;
                 break;
             }
         }
-        for (int i = number; i < size(); i++){
+        for (int i = number; i < size - 1; i++) {
             storage[i] = storage[i + 1];
         }
         storage[size] = null;
@@ -47,8 +48,8 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        Resume[] resumes = new Resume[size()];
-        for (int i = 0; i < size(); i++){
+        Resume[] resumes = new Resume[size];
+        for (int i = 0; i < size; i++) {
             resumes[i] = storage[i];
         }
         return resumes;
